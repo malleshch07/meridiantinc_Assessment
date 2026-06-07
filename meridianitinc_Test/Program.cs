c8b03a5149054fda7b539ae7738a410428c2a938
mallesh chitikena <mallesh.gohan7@gmail.com>
2026-06-07 16:07:43 +0530

    Add project files.

---
+++ meridianitinc_Test/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace meridianitinc_Assessment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
